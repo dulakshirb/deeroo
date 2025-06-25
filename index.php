cecc956c50828530ecab64f521ab9d7806a979a3
Dulakshi RB <dulakshirb@gmail.com>
2025-06-25 17:33:55 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Main template file.
+ * 
+ * @package Deeroo
+ */
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WorddPress Theme</title>
+ </head>
+ <body>
+    hello WorddPress
+ </body>
+ </html>
