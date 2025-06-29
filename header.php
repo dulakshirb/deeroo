4e4f6cb5f320d4cb38adc7ebc93a6c4fe1699865
Dulakshi RB <dulakshirb@gmail.com>
2025-06-29 08:46:41 +0530

    added wp_head() and wp_footer()

--- header.php
+++ header.php
@@ -4,14 +4,16 @@
  * 
  * @package Deeroo
  */
- ?>
+?>
 
- <!DOCTYPE html>
- <html lang="en">
- <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WorddPress Theme</title>
- </head>
- <body>
-<header>Header</header>
+<!DOCTYPE html>
+<html lang="<?php language_attributes(); ?>">
+
+<head>
+   <meta charset="<?php bloginfo('charset'); ?>">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <?php wp_head(); ?>
+</head>
+
+<body>
+   <header>Header</header>
