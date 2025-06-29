5546e5203d515e61c4bf5f66b22e673d043a9383
Dulakshi RB <dulakshirb@gmail.com>
2025-06-29 10:09:58 +0530

    enqueue scripts

--- functions.php
+++ functions.php
@@ -6,7 +6,8 @@
  */
 
  function deeroo_enqueue_scripts(){
-    wp_enqueue_style( 'style-sheet', get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css' ), 'all' );
+    wp_enqueue_style( 'style-css', get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css' ), 'all' );
+    wp_enqueue_script( 'main-js', get_template_directory_uri() . '/assets/main.js', [], filemtime(get_template_directory() . '/assets/main.js'), true );
  }
 
  add_action( "wp_enqueue_scripts", "deeroo_enqueue_scripts" );
