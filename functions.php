91d8f3671623fee8fdb0c7c705794d017d11421e
Dulakshi RB <dulakshirb@gmail.com>
2025-06-29 09:13:05 +0530

    enqueue stylesheet

--- functions.php
+++ functions.php
@@ -4,3 +4,9 @@
  * 
  * @package Deeroo
  */
+
+ function deeroo_enqueue_scripts(){
+    wp_enqueue_style( 'style-sheet', get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css' ), 'all' );
+ }
+
+ add_action( "wp_enqueue_scripts", "deeroo_enqueue_scripts" );
